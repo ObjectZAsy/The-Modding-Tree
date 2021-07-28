f0866b0f23403db6beb7dc5d41da6ac7de06590f
ObjectZAsy <objectzasy@gmail.com>
2021-07-28 15:11:19 -0400

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -62,8 +62,8 @@ addLayer("e", {
     symbol: "E", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
-        unlocked: true,
-		points: new Decimal(0),
+        unlocked: false,
+		points: new Decimal(100),
     }},
     color: "#FFFF80",
     requires: new Decimal(100), // Can be a function that takes requirement increases into account
